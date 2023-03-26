3505ba62ca374f7ca481b2f8163023ed63c1b912
Marco Filippone <m.filip@hotmail.it>
2023-03-26 12:35:45 +0200

    fix string client

--- custom-samples/distilbert/client.py
+++ custom-samples/distilbert/client.py
@@ -10,7 +10,7 @@ from tritonclient.utils import np_to_triton_dtype
 client = httpclient.InferenceServerClient(url="localhost:8001")
 
 # %%
-text = np.array([["This is a string"]], dtype=np.object_)
+text = np.array([["This is a string"]], dtype="object")
 
 input_tensor = httpclient.InferInput("text", text.shape, "BYTES")
 input_tensor.set_data_from_numpy(text)
@@ -43,3 +43,16 @@ res = requests.post(
     json=input_json
 ).json()
 pprint(res)
+
+"""
+{'model_name': 'preprocess',
+ 'model_version': '1',
+ 'outputs': [{'data': [101, 2023, 2003, 1037, 5164, 102],
+              'datatype': 'INT64',
+              'name': 'input_ids',
+              'shape': [1, 6]},
+             {'data': [1, 1, 1, 1, 1, 1],
+              'datatype': 'INT64',
+              'name': 'attention_mask',
+              'shape': [1, 6]}]}
+"""
